12c909c5e41d457dc73e35580c8206ee32619a1e
OscarAlberto <s245739@studenti.polito.it>
2020-05-25 08:22:29 +0200

    creazione grafo e aggiunta vertici

--- src/main/java/it/polito/tdp/extflightdelays/model/TestModel.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/TestModel.java
@@ -3,8 +3,10 @@ package it.polito.tdp.extflightdelays.model;
 public class TestModel {
 
 	public static void main(String[] args) {
-		
-		Model model = new Model();
+
+		Model m = new Model();
+
+		m.creaGrafo();
 
 	}
 
